e6d8afa1e5a2798defc9d3bce3219943278a684f
Martin Rowan <martin@rowannet.co.uk>
2019-12-26 23:25:06 +0000

    Minor cleanup

--- src/DogDoor.ino
+++ src/DogDoor.ino
@@ -6,15 +6,9 @@
  */
 
 /* 
- * TODO: Review use of onboard RGB LED: https://docs.particle.io/reference/device-os/firmware/photon/#rgb
- * TODO: Review if PRODUCT_ID and PRODUCT_VERSION need to be defined: https://docs.particle.io/reference/device-os/firmware/photon/#macros
- * TODO: Review what information makes sense to publish to Cloud: https://docs.particle.io/reference/device-os/firmware/photon/#cloud-functions
- * TODO: Measure presenece sensor outputs to ensure high voltage is not > 3v3
+ * TODO: Work without connection to cloud. Currently doesn't
+ * TODO: Measure presence sensor outputs to ensure high voltage is not > 3v3
  * TODO: Status LEDs connected to Analog Outputs?
- * TODO: Setup manual toggle switch
- * TODO: Setup manual trigger switch, or use button for system.reset or RST pin
- * TODO: Re-cable motors with sheilded wire
- * TODO: Review disabling OTA updates whilst motor is active: https://docs.particle.io/reference/device-os/firmware/photon/#ota-updates
  */
 
 SYSTEM_THREAD(ENABLED);  // Have Particle processing in a separate thread - https://docs.particle.io/reference/device-os/firmware/photon/#system-thread
@@ -70,7 +64,8 @@ const int moveFurtherIncrement = 10000;
 const int keepOpenTime = 10000;  // 10 seconds
 const int cloudPublishInterval = 30000;  // 30 seconds
 const int traceLogInterval = 5000;  // 5 second
-const int periodicLogInterval = 300000;  // 5 minutes
+// const int periodicLogInterval = 300000;  // 5 minutes
+const int periodicLogInterval = 30000;  //  30 seconds
 
 // Limit end stop positions
 int openPosition = 0;
@@ -100,7 +95,7 @@ Timer traceLogTimer(traceLogInterval, traceLog);
 Timer periodicLogTimer(periodicLogInterval, periodicLog);
 // Create timer to send current variable values to cloud for switch state
 Timer particleVarPublishTimer(cloudPublishInterval, publishVariables);
-// Timer for how long the door is kept open after no presense detected.
+// Timer for how long the door is kept open after no presence detected.
 Timer keepOpenTimer(keepOpenTime, timerCallback, true);
 
 /**********************************************************************************************************************
@@ -133,9 +128,9 @@ void setup() {
     pinMode(indoorSensor, INPUT_PULLDOWN);  // PULLDOWN - High Signal must not exceed 3v3
     pinMode(outdoorSensor, INPUT_PULLDOWN); // PULLDOWN - High Signal must not exceed 3v3
     pinMode(manualSwitch, INPUT_PULLDOWN); // PULLDOWN - High Signal must not exceed 3v3
-    attachInterrupt(indoorSensor, presenseSensorISR, RISING);
-    attachInterrupt(outdoorSensor, presenseSensorISR, RISING);
-    attachInterrupt(manualSwitch, presenseSensorISR, RISING);
+    attachInterrupt(indoorSensor, presenceSensorISR, RISING);
+    attachInterrupt(outdoorSensor, presenceSensorISR, RISING);
+    attachInterrupt(manualSwitch, presenceSensorISR, RISING);
 
     // Setup the toggle switch
     pinMode(keepOpenSwitch, INPUT_PULLDOWN);
@@ -155,6 +150,8 @@ void setup() {
         // We're already closed at satrtup. Set open to be the inverse of the normal closed pos.
         openPosition = -initialClosedPosition;
     }
+    byte mac[6];
+    WiFi.macAddress(mac);
 }
 
 
@@ -171,7 +168,7 @@ void limitSwitchISR() {
     }
 }
 
-void presenseSensorISR() {
+void presenceSensorISR() {
     if (!digitalRead(keepClosedSwitch)){
         desiredDoorState = OPEN;
         keepOpenTimer.changePeriodFromISR(keepOpenTime);  // Set or reset the timer
@@ -182,11 +179,19 @@ void presenseSensorISR() {
 /**********************************************************************************************************************
  * Functions
  *********************************************************************************************************************/
-boolean presenseDetected() {
+boolean presenceDetected() {
     // True if either sensor is active (high)
     if ( (digitalRead(indoorSensor)) or (digitalRead(outdoorSensor)) or (digitalRead(manualSwitch)) ) {
-        Log.trace("presenseDetected: Indoor: %d - Outdoor: %d - Manual: %d",
+        Log.trace("presenceDetected: Indoor: %d - Outdoor: %d - Manual: %d",
                    digitalRead(indoorSensor), digitalRead(outdoorSensor), digitalRead(manualSwitch));
+        if (publish) {
+            if (Particle.connected()) {
+                Particle.publish("Presence Detected: ",
+                                  String::format("Indoor: %d - Outdoor: %d - Manual: %d",
+                                  digitalRead(indoorSensor), digitalRead(outdoorSensor), digitalRead(manualSwitch)),
+                                  PRIVATE);
+            }
+        }
         return true;
     }
     return false;
@@ -212,6 +217,7 @@ void periodicLog() {
     Log.info("OS version: %s", (const char*)System.version());
     Log.info("Free Memory: %d", System.freeMemory());
     Log.info("Uptime (secs): %d", System.uptime());
+    Log.info("System restart reason: %d - Data: %d", System.resetReason(), System.resetReasonData());
     Log.info("Version: %s - Build: %s", (const char*)version, (const char*)buildDate);
 
     Log.info("Desired State: %d - Current State: %d", desiredDoorState, currentDoorState);
@@ -227,8 +233,8 @@ void publishVariables() {
 }
 
 void timerCallback() {
-    if (presenseDetected()) {
-        // Timer expired, but reset as presense detected
+    if (presenceDetected()) {
+        // Timer expired, but reset as presence detected
         Log.trace("timerCallback - Extending timer");
         keepOpenTimer.changePeriod(keepOpenTime);  //Set or reset the timer
     } else {
